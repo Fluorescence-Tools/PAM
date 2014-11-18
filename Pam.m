f61c9be4499c9b783aea6edb483cbdc7fffe60f6
Anders Barth <anders.barth@gmail.com>
2014-11-18 14:55:10 +0000

    BurstSearch
    git-svn-id: svn://10.153.116.2/PAM@19 cb18dcf6-275f-e045-ae08-2e8c34a2df07

--- Pam.m
+++ Pam.m
@@ -3792,6 +3792,189 @@ LSUserValues(1);
 %%% Performs a Burst Analysis  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function Do_BurstAnalysis()
+global FileInfo UserValues PamMeta
+%% Initialization
+h = guidata(gcf);
+%%% clear preview burst data still in workspace
+clearvars -global BurstData
+global BurstData
+
+%%% Set Progress Bar
+h.Progress_Text.String = 'Performing Burst Search...';
+h.Progress_Axes.Color=[1 0 0];
+drawnow;
+
+%% Burst Search
+%%% The Burst Search Procedure outputs three vectors containing the
+%%% Macrotime (AllPhotons), Microtime (AllPhotons_Microtime) and the
+%%% Channel as a Number (Channel) of all Photons in the PIE channels used
+%%% for the BurstSearch.
+%%% The Bursts are defined via the start and stop vectors, containing the
+%%% absolute photon number (NOT the macrotime) of the first and last photon
+%%% in a burst. Additonally, the BurstSearch puts out the Number of Photons
+%%% per Burst directly.
+
+%%% The Channel Information is encoded as follows:
+
+%%% 2color-MFD:
+%%% 1   2   GG1 GG2
+%%% 3   4   GR1 GR2
+%%% 5   6   RR1 RR2
+
+%%% 3color-MFD
+%%% 1   2   BB1 BB2
+%%% 3   4   BG1 BG2
+%%% 5   6   BR1 BR2
+%%% 7   8   GG1 GG2
+%%% 9   10  GR1 GR2
+%%% 11  12  RR1 RR2
+
+%%% 2color-noMFD
+%%% 1       GG
+%%% 2       GR
+%%% 3       RR
+
+BAMethod = UserValues.BurstSearch.Method;
+%achieve loading of less photons by using chunksize of preview and first
+%chunk
+ChunkSize = 30; %30 minutes, hard-coded for now
+Number_of_Chunks = ceil(FileInfo.MeasurementTime/(ChunkSize*60));
+for i = 1:Number_of_Chunks
+    Progress((i-1)/Number_of_Chunks);
+    if any(BAMethod == [1 2]) %ACBS 2 Color
+        %prepare photons
+        %read out macrotimes for all channels
+        Photons{1} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{1,1},'Macrotime',i,ChunkSize);
+        Photons{2} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{1,2},'Macrotime',i,ChunkSize);
+        Photons{3} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{2,1},'Macrotime',i,ChunkSize);
+        Photons{4} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{2,2},'Macrotime',i,ChunkSize);
+        Photons{5} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{3,1},'Macrotime',i,ChunkSize);
+        Photons{6} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{3,2},'Macrotime',i,ChunkSize);
+        AllPhotons_unsort = vertcat(Photons{:});
+        %sort macrotime and use index to sort microtime and channel
+        %information
+        [AllPhotons, index] = sort(AllPhotons_unsort);
+        clear AllPhotons_unsort
+        %get colors of photons
+        chan_temp = uint8([1*ones(1,numel(Photons{1})) 2*ones(1,numel(Photons{2})) 3*ones(1,numel(Photons{3}))...
+            4*ones(1,numel(Photons{4})) 5*ones(1,numel(Photons{5})) 6*ones(1,numel(Photons{6}))]);
+        Channel = chan_temp(index);
+        clear chan_temp
+        
+        %%% read out microtimes for all channels
+        MI{1} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{1,1},'Microtime',i,ChunkSize);
+        MI{2} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{1,2},'Microtime',i,ChunkSize);
+        MI{3} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{2,1},'Microtime',i,ChunkSize);
+        MI{4} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{2,2},'Microtime',i,ChunkSize);
+        MI{5} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{3,1},'Microtime',i,ChunkSize);
+        MI{6} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{3,2},'Microtime',i,ChunkSize);
+        
+        MI = vertcat(MI{:});
+        AllPhotons_Microtime = MI(index);
+        clear MI index
+        
+        if BAMethod == 1
+            T = UserValues.BurstSearch.SearchParameters{BAMethod}(2);
+            M = UserValues.BurstSearch.SearchParameters{BAMethod}(3);
+            L = UserValues.BurstSearch.SearchParameters{BAMethod}(1);
+            [start, stop, ~] = Perform_BurstSearch(AllPhotons,[],'APBS',T,M,L);
+        elseif BAMethod == 2
+            T = UserValues.BurstSearch.SearchParameters{BAMethod}(2);
+            M = [UserValues.BurstSearch.SearchParameters{BAMethod}(3),...
+                UserValues.BurstSearch.SearchParameters{BAMethod}(4)];
+            L = UserValues.BurstSearch.SearchParameters{BAMethod}(1);
+            [start, stop, ~] = Perform_BurstSearch(AllPhotons,Channel,'DCBS',T,M,L);
+        end
+    elseif any(BAMethod == [3,4])
+        Photons{1} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{1,1},'Macrotime',i,ChunkSize);
+        Photons{2} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{1,2},'Macrotime',i,ChunkSize);
+        Photons{3} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{2,1},'Macrotime',i,ChunkSize);
+        Photons{4} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{2,2},'Macrotime',i,ChunkSize);
+        Photons{5} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{3,1},'Macrotime',i,ChunkSize);
+        Photons{6} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{3,2},'Macrotime',i,ChunkSize);
+        Photons{7} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{4,1},'Macrotime',i,ChunkSize);
+        Photons{8} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{4,2},'Macrotime',i,ChunkSize);
+        Photons{9} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{5,1},'Macrotime',i,ChunkSize);
+        Photons{10} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{5,2},'Macrotime',i,ChunkSize);
+        Photons{11} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{6,1},'Macrotime',i,ChunkSize);
+        Photons{12} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{6,2},'Macrotime',i,ChunkSize);
+        AllPhotons_unsort = vertcat(Photons{:});
+        %sort
+        [AllPhotons, index] = sort(AllPhotons_unsort);
+        clear AllPhotons_unsort
+        %get colors of photons
+        chan_temp = uint8([1*ones(1,numel(Photons{1})) 2*ones(1,numel(Photons{2})) 3*ones(1,numel(Photons{3}))...
+            4*ones(1,numel(Photons{4})) 5*ones(1,numel(Photons{5})) 6*ones(1,numel(Photons{6}))...
+            7*ones(1,numel(Photons{7})) 8*ones(1,numel(Photons{8})) 9*ones(1,numel(Photons{9}))...
+            10*ones(1,numel(Photons{10})) 11*ones(1,numel(Photons{11})) 12*ones(1,numel(Photons{12}))]);
+        Channel = chan_temp(index);
+        clear chan_temp
+        
+        %%% read out microtimes
+        MI{1} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{1,1},'Microtime',i,ChunkSize);
+        MI{2} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{1,2},'Microtime',i,ChunkSize);
+        MI{3} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{2,1},'Microtime',i,ChunkSize);
+        MI{4} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{2,2},'Microtime',i,ChunkSize);
+        MI{5} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{3,1},'Microtime',i,ChunkSize);
+        MI{6} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{3,2},'Microtime',i,ChunkSize);
+        MI{7} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{4,1},'Microtime',i,ChunkSize);
+        MI{8} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{4,2},'Microtime',i,ChunkSize);
+        MI{9} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{5,1},'Microtime',i,ChunkSize);
+        MI{10} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{5,2},'Microtime',i,ChunkSize);
+        MI{11} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{6,1},'Microtime',i,ChunkSize);
+        MI{12} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{6,2},'Microtime',i,ChunkSize);
+        
+        MI = vertcat(MI{:});
+        AllPhotons_Microtime = MI(index);
+        clear MI index
+        
+        if BAMethod == 3 %ACBS 3 Color
+            T = UserValues.BurstSearch.SearchParameters{BAMethod}(2);
+            M = UserValues.BurstSearch.SearchParameters{BAMethod}(3);
+            L = UserValues.BurstSearch.SearchParameters{BAMethod}(1);
+            [start, stop, ~] = Perform_BurstSearch(AllPhotons,[],'APBS',T,M,L);
+        elseif BAMethod == 4 %TCBS
+            T = UserValues.BurstSearch.SearchParameters{BAMethod}(2);
+            M = [UserValues.BurstSearch.SearchParameters{BAMethod}(3),...
+                UserValues.BurstSearch.SearchParameters{BAMethod}(4),...
+                UserValues.BurstSearch.SearchParameters{BAMethod}(5)];
+            L = UserValues.BurstSearch.SearchParameters{BAMethod}(1);
+            [start, stop, ~] = Perform_BurstSearch(AllPhotons,Channel,'TCBS',T,M,L);
+        end
+        
+    elseif BAMethod == 5 %2 color no MFD
+        %prepare photons
+        %read out macrotimes for all channels
+        Photons{1} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{1,1},'Macrotime',i,ChunkSize);
+        Photons{2} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{2,1},'Macrotime',i,ChunkSize);
+        Photons{3} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{3,1},'Macrotime',i,ChunkSize);
+        AllPhotons_unsort = horzcat(Photons{:});
+        %sort
+        [AllPhotons, index] = sort(AllPhotons_unsort);
+        clear AllPhotons_unsort
+        %get colors of photons
+        chan_temp = uint8([1*ones(1,numel(Photons{1})) 2*ones(1,numel(Photons{2})) 3*ones(1,numel(Photons{3}))]);
+        Channel = chan_temp(index);
+        clear chan_temp
+        
+        %%%read out microtimes
+        MI{1} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{1,1},'Microtime',i,ChunkSize);
+        MI{2} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{2,1},'Microtime',i,ChunkSize);
+        MI{3} = Get_Photons_from_PIEChannel(UserValues.BurstSearch.PIEChannelSelection{BAMethod}{3,1},'Microtime',i,ChunkSize);
+       
+        MI = vertcat(MI{:});
+        AllPhotons_Microtime = MI(index);
+        clear MI index
+        
+        %do search
+        T = UserValues.BurstSearch.SearchParameters{BAMethod}(2);
+        M = UserValues.BurstSearch.SearchParameters{BAMethod}(3);
+        L = UserValues.BurstSearch.SearchParameters{BAMethod}(1);
+        [start, stop, ~] = Perform_BurstSearch(AllPhotons,[],'APBS',T,M,L);
+    end
+    %%% Process Data for this Chunk
+    
+end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Performs a Burst Search with specified algorithm  %%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
