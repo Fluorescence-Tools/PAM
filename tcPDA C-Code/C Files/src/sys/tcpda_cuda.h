688b682a145f3640b82c1b3315ed686287d4adf2
Anders Barth <anders.barth@gmail.com>
2015-05-04 12:01:46 +0000

    New eval_prob_3c_bg function written in C with CUDA (GPU) support.
    git-svn-id: svn://10.153.116.2/PAM@308 cb18dcf6-275f-e045-ae08-2e8c34a2df07

---
+++ tcPDA C-Code/C Files/src/sys/tcpda_cuda.h
@@ -0,0 +1,50 @@
+/* sys/tcpda.h
+ *
+ *  Contains tcPDA data for 3 Color MFD probability evaluation
+ *
+ */
+ 
+#ifndef TCPDA_H
+#define TCPDA_H
+
+#include <stdlib.h>									/* for size_t */
+
+typedef struct tcpda_data_struct {
+	float *fbb;        /* Bursts */
+	float *fbg;
+	float *fbr;
+	float *fgg;
+	float *fgr;
+	
+	float *NBGbb;       /* NBGbb = numel(BGbb)-1 */
+	float *NBGbg;
+	float *NBGbr;
+	float *NBGgg;
+	float *NBGgr;
+	
+	float *BGbb;       /* Background corrections */
+	float *BGbg;
+	float *BGbr;
+	float *BGgg;
+	float *BGgr;
+	
+	float *p_bb;       /* percentage of 'PBB = Pout_B./P_total;' */
+	float *p_bg;       
+	float *p_gr;
+    
+    float *out_matrix_device;       /* storage for computation */
+    float *P_binomial;
+    float *P_trinomial;
+    	
+	int *burst_cols;        /* Dimension of Burst Array */
+	int *burst_rows;
+	
+	int *p_cols;            /* Dimension of P Array */
+	int *p_rows;
+    
+    int *thread_count;   /* number of threads on the gpu */
+    int *block_count;
+} tcpda_data;
+//typedef struct tcpda_data_struct tcpda_data;
+
+#endif /* TCPDA_H */
