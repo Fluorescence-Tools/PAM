58a7a88da1b46de13dc17130b0e71e2fd00032c8
Anders Barth <anders.barth@gmail.com>
2015-05-15 10:57:18 +0000

    MHsample for flexible parameter choice
    git-svn-id: svn://10.153.116.2/PAM@319 cb18dcf6-275f-e045-ae08-2e8c34a2df07

--- tcPDA C-Code/MHsample.m
+++ tcPDA C-Code/MHsample.m
@@ -1,4 +1,4 @@
-function [samples,prob,acceptance] =  MHsample(nsamples,probfun,priorfun,sigma_prop,lb,ub,initial_parameters,Display)
+function [samples,prob,acceptance] =  MHsample(nsamples,probfun,priorfun,sigma_prop,lb,ub,initial_parameters,fixed,plot_params,param_names)
 %%% Performs Metropolis-Hastings-Sampling of posterior distribution
 %%% Input parameters:
 %%% nsamples    -   Number of Samples to draw
@@ -19,8 +19,11 @@ function [samples,prob,acceptance] =  MHsample(nsamples,probfun,priorfun,sigma_p
 %%%                 '2Gauss'    (Two Gaussian)
 %%%                 If left empty, no plot will be displayed.
 %%% initial_parameters - Vector of start parameters.
-%%% plotfun     -   optional plot function:
-%%%                 plotfun(samples,prob,acceptance,count)
+%%% fixed       -   logical array specifying which parameters to vary
+%%% plot_param  -   logical array of parameter values to plot each
+%%%                 iteration
+%%%                 (uses plotfun(samples,prob,acceptance,count) )
+%%% param_names -   Names of the parameters (Cell array) used for plotting
 %%%
 %%% Output parameters:
 %%% samples     -   An (n x m)array of the drawn samples with dimension
@@ -29,9 +32,12 @@ function [samples,prob,acceptance] =  MHsample(nsamples,probfun,priorfun,sigma_p
 %%% acceptance  -   The total acceptance rate of the proposed steps.
 
 %%% check whether to display plots or not
-if isempty(Display)
+if nargin < 8
     Display = 0;
+else
+    Display = 1;
 end
+
 %%% define global Stop variable to interrupt the algorithm by button press
 global Stop
 Stop = 0;
@@ -39,7 +45,7 @@ Stop = 0;
 rng('shuffle')
 
 %%% initialize output variables
-samples = zeros(nsamples,size(initial_parameters,2));
+samples = zeros(nsamples,size(initial_parameters,1));
 samples(1,:) = initial_parameters;
 prob = zeros(nsamples,1);
 
@@ -54,13 +60,14 @@ acc = 0;   %%% the number of successfull steps
 
 %%% Initalize plots
 if Display ~= 0
-    UpdatePlot(samples,prob,0,1,Display);
+    UpdatePlot(samples,prob,acc,count,plot_params,param_names);
 end
 %%% Start while loop
 %%% Loop stops if the number of samples was drawn or STOP was pressed
 while count < (nsamples) && (Stop == 0)
     %%% draw new parameters
-    param = normrnd(samples(count,:),sigma_prop);
+    param = samples(count,:);
+    param(~fixed) = normrnd(samples(count,(~fixed)),sigma_prop(~fixed));
     %%% increase count variable
     count = count + 1;
     %%% Apply boundaries but recycle random numbers (reflecting boundary)
@@ -101,14 +108,14 @@ while count < (nsamples) && (Stop == 0)
             prob(count) = Posterior_new;
             acceptance = acc/count;
             if Display ~= 0
-                UpdatePlot(samples,prob,acceptance,count,Display);
+                UpdatePlot(samples,prob,acceptance,count,plot_params,param_names);
             end
         else %%% value not accepted based on posterior, keep old value
             samples(count,:) = samples(count-1,:);
             prob(count) = prob(count-1);
             acceptance = acc/count;
             if Display ~= 0
-                UpdatePlot(samples,prob,acceptance,count,Display);
+                UpdatePlot(samples,prob,acceptance,count,plot_params,param_names);
             end
         end
     else %%% value not accepted based on prior, keep old value
@@ -116,7 +123,7 @@ while count < (nsamples) && (Stop == 0)
         prob(count) = prob(count-1);
         acceptance = acc/count;
         if Display ~= 0
-            UpdatePlot(samples,prob,acceptance,count,Display);
+            UpdatePlot(samples,prob,acceptance,count,plot_params,param_names);
         end
     end
 end
@@ -128,129 +135,48 @@ if Stop == 1
     prob( (count+1):end) = [];
 end
 
-function UpdatePlot(samples,prob,acceptance,count,Display)
+function UpdatePlot(samples,prob,acceptance,count,plot_params,param_names)
 %%% initializes or updates plot to show the chain progress
+n_param = sum(plot_params);
 
-%%% check whether to display at all
-if Display == 0
-    return;
-end
-
-switch Display
-    case '1Gauss'
-        %%% check if a figure exists
-        h = findobj('Tag','MHplot_1Gauss');
-        if isempty(h) %%% create new figure, depending on the model
-            %%% initialize plots
-            handles.Figure=figure('Tag',Display); 
-            subplot(3,3,[1 2 3]);
-            handles.plot_p = plot([prob(1:count)]);
-            subplot(3,3,4);
-            handles.plot_r = plot([samples(1:count,1)]);
-            subplot(3,3,7);
-            handles.plot_s = plot([samples(1:count,2)]);
-            subplot(3,3,[ 5 6 8 9])
-            handles.plot_rs = scatter(samples(1:count,1),samples(1:count,2),'filled');
-            handles.button = uicontrol('Style','pushbutton','Parent',handles.Figure,'Units','normalized',...
-                'Position',[0 0 0.1 0.1],'Callback',@StopCallback,'String','Stop');
-            handles.text = uicontrol('Style','text','String','0','Units','normalized','Position',[0.9 0 0.1 0.05]);
-            drawnow;
-            %%% save handles structure to guidata
-            guidata(gcf,handles);
-        else
-            handles = guidata(gcf);
-             %%% update plots
-            handles.plot_p.YData = prob;
-            handles.plot_r.YData = samples(1:count,1);
-            handles.plot_s.YData = samples(1:count,2);
-            handles.plot_rs.XData = samples(1:count,1);
-            handles.plot_rs.YData = samples(1:count,2);
-            handles.text.String = num2str(acceptance);
-            %plot_rs.CData = linspace(1,10,count);
-            drawnow;
+h = findobj('Tag','MCMC_Plot');
+if isempty(h) %%% create new figure, depending on the model
+    %%% initialize plots
+    handles.Figure=figure('Tag','MCMC_Plot','Units','normalized','Position',[0.2 0.1 0.6 0.8]); 
+    subplot(n_param+1,1,1);
+    handles.plot_p = plot(prob(1:count));
+    ylabel('log likelihood');
+    j = 1;
+    for i = 1:numel(plot_params)
+        if plot_params(i)
+            subplot(n_param+1,1,j+1);
+            handles.plot_param(j) = plot(samples(1:count,j));
+            ylabel(param_names(i));
+            j = j+1;
         end
-    case '2Gauss'
-        %%% check if a figure exists
-        h = findobj('Tag',Display);
-        if isempty(h) %%% create new figure, depending on the model
-            %%% initialize plots
-            handles.Figure=figure('Tag',Display); 
-            subplot(3,3,[1 2]);
-            handles.plot_p = plot([prob(1:count)]);
-            subplot(3,3,3);
-            handles.plot_a = plot([samples(1:count,5)]);
-            subplot(3,3,4);
-            handles.plot_r1 = plot([samples(1:count,1)]);hold on;
-            handles.plot_r2 = plot([samples(1:count,3)]);hold off;
-            subplot(3,3,7);
-            handles.plot_s1 = plot([samples(1:count,2)]);hold on;
-            handles.plot_s2 = plot([samples(1:count,4)]);hold off;
-            subplot(3,3,[ 5 6 8 9])
-            handles.plot_r1s1 = scatter(samples(1:count,1),samples(1:count,2),'b','filled');hold on;
-            handles.plot_r2s2 = scatter(samples(1:count,3),samples(1:count,4),'r','filled');hold off;
-            handles.button = uicontrol('Style','pushbutton','Parent',handles.Figure,'Units','normalized',...
-                'Position',[0 0 0.1 0.1],'Callback',@StopCallback,'String','Stop');
-            handles.text = uicontrol('Style','text','String','0','Units','normalized','Position',[0.9 0 0.1 0.05]);
-            drawnow;
-            %%% save handles structure to guidata
-            guidata(gcf,handles);
-        else
-            handles = guidata(gcf);
-            %%% update plots
-            handles.plot_p.YData = prob(1:count);
-            handles.plot_r1.YData = samples(1:count,1);
-            handles.plot_s1.YData = samples(1:count,2);
-            handles.plot_r1s1.XData = samples(1:count,1);
-            handles.plot_r1s1.YData = samples(1:count,2);
-            handles.plot_r2.YData = samples(1:count,3);
-            handles.plot_s2.YData = samples(1:count,4);
-            handles.plot_r2s2.XData = samples(1:count,3);
-            handles.plot_r2s2.YData = samples(1:count,4);
-            handles.plot_a.YData = samples(1:count,5);
-            %plot_rs.CData = linspace(1,10,count);
-            handles.text.String = num2str(acceptance);
-            drawnow;
-        end 
-    case 'tcPDA_mcmc'
-        %%% check if a figure exists
-        h = findobj('Tag',Display);
-        if isempty(h) %%% create new figure, depending on the model
-            %%% initialize plots
-            handles.Figure=figure('Tag',Display); 
-            subplot(2,4,[1 5]);
-            handles.plot_p = plot([prob(1:count)]);
-            subplot(2,4,2);
-            handles.plot_rbg = plot([samples(1:count,1)]);
-            subplot(2,4,3);
-            handles.plot_rbr = plot([samples(1:count,3)]);
-            subplot(2,4,4);
-            handles.plot_rgr = plot([samples(1:count,5)]);
-            subplot(2,4,6);
-            handles.plot_sbg = plot([samples(1:count,2)]);
-            subplot(2,4,7);
-            handles.plot_sbr = plot([samples(1:count,4)]);
-            subplot(2,4,8);
-            handles.plot_sgr = plot([samples(1:count,6)]);
-            handles.button = uicontrol('Style','pushbutton','Parent',handles.Figure,'Units','normalized',...
-                'Position',[0 0 0.1 0.1],'Callback',@StopCallback,'String','Stop');
-            handles.text = uicontrol('Style','text','String','0','Units','normalized','Position',[0.9 0 0.1 0.05]);
-            drawnow;
-            %%% save handles structure to guidata
-            guidata(gcf,handles);
-        else
-            handles = guidata(gcf);
-            %%% update plots
-            handles.plot_p.YData = prob(1:count);
-            handles.plot_rbg.YData = samples(1:count,1);
-            handles.plot_sbg.YData = samples(1:count,2);
-            handles.plot_rbr.YData = samples(1:count,3);
-            handles.plot_sbr.YData = samples(1:count,4);
-            handles.plot_rgr.YData = samples(1:count,5);
-            handles.plot_sgr.YData = samples(1:count,6);
-            handles.text.String = num2str(acceptance);
-            drawnow;
-        end 
-end
+    end
+    handles.button = uicontrol('Style','pushbutton','Parent',handles.Figure,'Units','normalized',...
+        'Position',[0 0 0.1 0.1],'Callback',@StopCallback,'String','Stop');
+    handles.text = uicontrol('Style','text','String','0','Units','normalized','Position',[0.9 0 0.1 0.05]);
+    drawnow;
+    %%% save handles structure to guidata
+    guidata(gcf,handles);
+else
+    handles = guidata(gcf);
+    %%% update plots
+    handles.plot_p.YData = prob(1:count);
+    j = 1;
+    for i = 1:numel(plot_params)
+        if plot_params(i)
+            handles.plot_param(j).YData = samples(1:count,j);
+            j = j+1;
+        end
+        
+    end
+    handles.text.String = num2str(acceptance);
+    drawnow;
+end 
+
 
 function StopCallback(~,~)
 global Stop
