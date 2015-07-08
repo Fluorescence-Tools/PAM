346029502d258bd91daa52d7fe596d351a4d93cd
Jelle <Jelle@mycompany.com>
2015-07-08 16:23:32 +0000

    moved generatename to pam folder
    git-svn-id: svn://10.153.116.2/PAM@410 cb18dcf6-275f-e045-ae08-2e8c34a2df07

--- Pam.m
+++ Pam.m
@@ -6454,44 +6454,6 @@ end
 LSUserValues(1)
 
 
-
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% Generates windows-compatible file names and adds increment %%%%%%%%%%%%
-%%% when overwriting %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function filename = GenerateName(filename)
-% works only if extension == 3 digits!
-
-
-% take only the outer parts of the filename if filename is too long (> 256 characters, path + name) for windows
-if numel(filename) > 251
-    disp('Sum of filename+filepath was too long for windows. Center part of filename was deleted.')
-    a = find(filename == filesep);
-    filestart = filename(1:a(end));
-    fileend = filename(a(end)+1:end);
-    while numel(filename) > 251
-        if numel(fileend) > 30
-            %keep the outer parts of the filename
-            fileend = [fileend(1:floor(end/2)-5) fileend(floor(end/2)+5:end)];
-            filename = [filestart fileend];
-        else
-            msgbox('FileName cannot be shortened, move file some folders up to render the "Filename + filepath" string shorter than 256 characters')
-            return
-        end
-    end
-end
-% add an underscore if filename existed
-if exist(filename, 'file')
-    k = 1;
-    while exist([filename(1:end-4) '_' num2str(k) filename(end-3:end)], 'file')
-        k = k+1;
-    end
-    filename = [filename(1:end-4) '_' num2str(k) filename(end-3:end)];
-end
-
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Functions concerning database of quick access filenames %%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
