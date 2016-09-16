dbac44fda68dda0b90c3e8e68b21968a89cf9ce3
Anders Barth <anders.barth@gmail.com>
2016-09-16 09:01:56 +0000

    Added iconbutton function. Added visualization of donor only lifetime determinaton in burstbrowser.
    git-svn-id: svn://10.153.116.2/PAM@1181 cb18dcf6-275f-e045-ae08-2e8c34a2df07

---
+++ functions/iconbutton.m
@@ -0,0 +1,9 @@
+function iconbutton(hBut,imgFile)
+%%% plots scaled image on button
+[a,~]=imread(imgFile);
+hBut.Units = 'pixels';
+wid = floor(hBut.Position(3));
+hei = floor(hBut.Position(4));
+a = imresize(a, [hei wid]);
+set(hBut,'CData',a);
+hBut.Units = 'normalized';
