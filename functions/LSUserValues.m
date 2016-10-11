a5ec5b207a5ec2270cc3edc172ac56e69dd0f734
Anders Barth <anders.barth@gmail.com>
2016-10-11 14:14:05 +0000

    git-svn-id: svn://10.153.116.2/PAM@1261 cb18dcf6-275f-e045-ae08-2e8c34a2df07

--- functions/LSUserValues.m
+++ functions/LSUserValues.m
@@ -879,7 +879,7 @@ if Mode==0 %%% Loads user values
     % 20 sigR
     % 21 FD0
     % FitParams{chan}(n) with chan the GG/RR or BB/GG/RR channel and n the parameter index
-    if ~isfield (S.TauFit,'FitParams') || (numel(S.TauFit.FitParams) ~= 4)
+    if ~isfield (S.TauFit,'FitParams') %|| (numel(S.TauFit.FitParams) ~= 4)
         params =      [2 2 2 0.5 0.5 0 0 0 0 0 50 2 0 0 1 1 0.4 0 50 5 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0];
         fix = logical([0 0 0 0   0   1 1 1 1 1 1  1 1 1 0 0 0   0 0  0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]);
         S.TauFit.FitParams = {params,params,params,params};
