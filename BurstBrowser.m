004cfeb94797c7eead990e305df6125017e86ca0
Anders Barth <anders.barth@gmail.com>
2016-12-05 15:55:42 +0000

    update to result display in tcPDA. moved jetvar colormap to its own function.
    git-svn-id: svn://10.153.116.2/PAM@1446 cb18dcf6-275f-e045-ae08-2e8c34a2df07

--- BurstBrowser.m
+++ BurstBrowser.m
@@ -13141,36 +13141,6 @@ while mod(numel(invec),newbin) ~= 0
 end
 outv = sum(reshape(invec,newbin,numel(invec)/newbin),1)';
 
-function out = jetvar(m)
-
-% JETVAR Variant of Jet colormap.
-%
-% Usage: OUT = JETVAR(M)
-%
-% This returns an M-by-3 matrix containing a variant of the Jet colormap.
-% Instead of starting at dark blue as Jet does, it starts at white. It goes
-% to pure blue from white, and then continues exactly as Jet does, ranging
-% through shades blue, cyan, green, yellow, and red, and ending with dark
-% red. M should be at least 10 to ensure there is at least one white color.
-%
-% Inputs:
-%   -M: Length of colormap (optional, default is the length of the current
-%   figure's colormap).
-%
-% Outputs:
-%   -OUT: M-by-3 colormap.
-%
-% See also: JET, HSV, HOT, PINK, FLAG, COLORMAP, RGBPLOT.
-
-if nargin < 1
-    m = size(get(gcf, 'colormap'), 1);
-end
-out = jet(m);
-% Modify the output starting at 1 before where Jet outputs pure blue.
-n = find(out(:, 3) == 1, 1) - 1;
-out(1:n, 1:2) = repmat((n:-1:1)'/n, [1 2]);
-out(1:n, 3) = 1;
-
 function model = MultiGaussFit_1D(x,xdata)
 global BurstMeta
 xbins = xdata{1};
