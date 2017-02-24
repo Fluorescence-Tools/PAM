2d166a273f001002c9105901590b82117bcb6b49
Anders Barth <anders.barth@gmail.com>
2017-02-24 17:38:40 +0000

    git-svn-id: svn://10.153.116.2/PAM@1569 cb18dcf6-275f-e045-ae08-2e8c34a2df07

--- FCSFit.m
+++ FCSFit.m
@@ -1573,6 +1573,14 @@ end
 if (Min == Max)
     Max = Min+1;
 end
+%%% get maximum x value
+maxTime = 0;
+for i = 1:numel(FCSData.Data)
+    maxTime = max([maxTime,FCSData.Data{i}.Cor_Times(end)]);
+end
+if (Max > maxTime)
+    Max = maxTime;
+end
 h.Fit_Min.String = num2str(Min);
 h.Fit_Max.String = num2str(Max);
 UserValues.FCSFit.Fit_Min = Min;
