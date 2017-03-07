c0b5e57a67c0069b967959725b52bbc50bf43b88
Anders Barth <anders.barth@gmail.com>
2017-03-07 16:51:59 +0000

    git-svn-id: svn://10.153.116.2/PAM@1592 cb18dcf6-275f-e045-ae08-2e8c34a2df07

--- functions/BurstBrowser/unimplemented/burstwise_traces_2c.m
+++ functions/BurstBrowser/unimplemented/burstwise_traces_2c.m
@@ -28,7 +28,7 @@ for i = 1:numel(Mac)
     hRR{i} = hist(mRRt{i},0:1:maxi(i));
 end
 
-i=2;t
+i=2;
 figure;plot(hGG{i},'g');hold on;plot(hGR{i},'r');plot(hRR{i},'k')
 figure;plot(hGR{i}./(hGG{i}+hGR{i}),'b');hold on;plot((hGG{i}+hGR{i})./(hRR{i}+hGG{i}+hGR{i}),'r');
 
