2d441e6ad99fa7ac940e77a3df04aa7c900f1286
PDA <PDA@Giulia-PC>
2017-03-29 13:22:59 +0200

    Added test file to resolve white space issues.

---
+++ test.m
@@ -0,0 +1,10 @@
+This is a test file for checking line ending and whitespace issues between
+windows and unix.
+
+This line has 5 trailing white space characters.     
+
+This line has no trailing white space.
+
+The next line is only white space.
+                                   
+Test end
