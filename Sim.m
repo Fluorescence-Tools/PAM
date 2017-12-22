ec969490f237a837336d158f9b2924edd55b5526
Anders Barth <anders.barth@gmail.com>
2017-12-22 12:15:07 +0100

    Fixed a bug on startup.

--- Sim.m
+++ Sim.m
@@ -1912,7 +1912,7 @@ SimData.General(File).Species = SimData.Species;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function Update_Concentration(Sel)
 global SimData
-h = guidata(gcbo);
+h = guidata(findobj('Tag','Sim'));
 c = SimData.Species(Sel).N./prod(SimData.General.BS)./1E-24./6.022E23./1E-9;
 h.Sim_Concentration.String = sprintf('= %.2f nM',c);
 
