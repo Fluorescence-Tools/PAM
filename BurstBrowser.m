246e3c01287b475e594dca1f1508266a231007c1
Anders <anders.barth@gmail.com>
2018-09-11 11:22:35 +0200

    BurstBrowser: Added phasor plots.

--- BurstBrowser.m
+++ BurstBrowser.m
@@ -4319,6 +4319,19 @@ for i = 1:numel(BurstData)
 end
 BurstMeta.SelectedFile = 1;
 
+%%% reset lifetime ind selection to normal
+if numel(h.lifetime_ind_popupmenu.String) > 4
+    h.lifetime_ind_popupmenu.String = h.lifetime_ind_popupmenu.String(1:4);
+    h.lifetime_ind_popupmenu.Value = 1;
+end
+%%% check if phasor data is present
+if any(strcmp(BurstData{1}.NameArray,'Phasor: gD'))
+    h.lifetime_ind_popupmenu.String{end+1} = '<html>g<sub>D</sub> vs s<sub>D</sub></html>'
+end
+if any(strcmp(BurstData{1}.NameArray,'Phasor: gA'))
+    h.lifetime_ind_popupmenu.String{end+1} = '<html>g<sub>A</sub> vs s<sub>A</sub></html>'
+end
+    
 %%% Set Parameter list after all parameters are defined
 set(h.ParameterListX, 'String', BurstData{1}.NameArray);
 set(h.ParameterListX, 'Value', posE);
@@ -12658,6 +12671,12 @@ switch BurstData{file}.BAMethod
                 origin = h.axes_rRRvsTauRR;
                 paramX = 'Lifetime A [ns]';
                 paramY = 'Anisotropy A';
+            case 5 % Phasor plot of g_d vs s_d
+                paramX = 'Phasor: gD';
+                paramY = 'Phasor: sD';
+            case 6 % Phasor plot of g_a vs s_a
+                paramX = 'Phasor: gA';
+                paramY = 'Phasor: sA';
         end
     case {3,4}
         switch h.lifetime_ind_popupmenu.Value
@@ -12697,24 +12716,106 @@ for i=1:numel(BurstMeta.Plots.MultiScatter.h1dy_lifetime)
 end
 legend(h.axes_lifetime_ind_1d_x,'off');
 cla(h.axes_lifetime_ind_2d);
-plots =origin.Children;
-for i = numel(plots):-1:1
-    handle_temp = copyobj(plots(i),h.axes_lifetime_ind_2d);
-    type{i} = plots(i).Type;
-    handle_temp.UIContextMenu = h.ExportGraphLifetime_Menu;
-end
 
-h.axes_lifetime_ind_2d.XLim = origin.XLim;
-h.axes_lifetime_ind_2d.YLim = origin.YLim;
-h.axes_lifetime_ind_2d.XLabel.String = origin.XLabel.String;
-h.axes_lifetime_ind_2d.XLabel.Color = UserValues.Look.Fore;
-h.axes_lifetime_ind_2d.YLabel.String = origin.YLabel.String;
-h.axes_lifetime_ind_2d.YLabel.Color = UserValues.Look.Fore;
-h.axes_lifetime_ind_2d.CLimMode = 'auto';h.axes_lifetime_ind_2d.CLim(1) = 0;
-%%% find the image plot
-xdata = plots(strcmp(type,'image')).XData;
-ydata = plots(strcmp(type,'image')).YData;
-zdata = plots(strcmp(type,'image')).CData;
+if exist('origin','var') %%% simply copy the plots
+    plots =origin.Children;
+    for i = numel(plots):-1:1
+        handle_temp = copyobj(plots(i),h.axes_lifetime_ind_2d);
+        type{i} = plots(i).Type;
+        handle_temp.UIContextMenu = h.ExportGraphLifetime_Menu;
+    end
+
+    h.axes_lifetime_ind_2d.XLim = origin.XLim;
+    h.axes_lifetime_ind_2d.YLim = origin.YLim;
+    h.axes_lifetime_ind_2d.XLabel.String = origin.XLabel.String;
+    h.axes_lifetime_ind_2d.XLabel.Color = UserValues.Look.Fore;
+    h.axes_lifetime_ind_2d.YLabel.String = origin.YLabel.String;
+    h.axes_lifetime_ind_2d.YLabel.Color = UserValues.Look.Fore;
+    h.axes_lifetime_ind_2d.CLimMode = 'auto';h.axes_lifetime_ind_2d.CLim(1) = 0;
+    %%% find the image plot
+    xdata = plots(strcmp(type,'image')).XData;
+    ydata = plots(strcmp(type,'image')).YData;
+    zdata = plots(strcmp(type,'image')).CData;
+elseif ~isempty(strfind(paramX,'Phasor')) %%% phasor plot
+    %%% data needs to be read out from the parameters
+    idx_x = find(strcmp(BurstData{file}.NameArray,paramX));
+    idx_y = find(strcmp(BurstData{file}.NameArray,paramY));
+    %%% Read out the Number of Bins
+    nbinsX = UserValues.BurstBrowser.Display.NumberOfBinsX;
+    nbinsY = UserValues.BurstBrowser.Display.NumberOfBinsY;
+    %%% set limits
+    x_lim = [-0.1,1.1];
+    y_lim = [0, 0.75];
+    if ~h.MultiselectOnCheckbox.UserData
+        datatoplot = BurstData{file}.DataCut;
+        [H, xbins, ybins] = calc2dhist(datatoplot(:,idx_x), datatoplot(:,idx_y),[nbinsX nbinsY], x_lim, y_lim);
+        datapoints = [datatoplot(:,idx_x), datatoplot(:,idx_y)];
+    else
+        [H,xbins,ybins,~,~,datapoints,n_per_species] = MultiPlot([],[],h,NameArray{idx_x},NameArray{idx_y},{x_lim, y_lim});
+    end
+    %%% copy the E vs tauA plot as template
+    origin =  h.axes_EvsTauRR;
+    plots = origin.Children;
+    for i = numel(plots):-1:1
+        handle_temp = copyobj(plots(i),h.axes_lifetime_ind_2d);
+        type{i} = plots(i).Type;
+        handle_temp.UIContextMenu = h.ExportGraphLifetime_Menu;
+    end
+    c = flipud(h.axes_lifetime_ind_2d.Children);
+    c(1).XData = xbins;
+    c(1).YData = ybins;
+    c(1).CData = H;
+    if ~UserValues.BurstBrowser.Display.KDE
+        c(1).AlphaData = (H>0);
+    elseif UserValues.BurstBrowser.Display.KDE
+        c(1).AlphaData = (H./max(max(H)) > 0.01);%ones(size(H,1),size(H,2));
+    end
+    c(2).XData = [xbins(1)-min(diff(xbins)),xbins,xbins(end)+min(diff(xbins))];
+    c(2).YData = [ybins(1)-min(diff(ybins)),ybins,ybins(end)+min(diff(ybins))];
+    H = H/max(max(H));Hcontour =zeros(size(H)+2); Hcontour(2:end-1,2:end-1) = H;
+    % replicate to fix edges
+    Hcontour(2:end-1,1) = H(:,1);Hcontour(2:end-1,end) = H(:,end);Hcontour(1,2:end-1) = H(1,:);Hcontour(end,2:end-1) = H(end,:);
+    Hcontour(1,1) = H(1,1);Hcontour(end,1) = H(end,1);Hcontour(1,end) = H(1,end);Hcontour(end,end) = H(end,end);
+    c(2).ZData = Hcontour;
+    c(2).LevelList = linspace(UserValues.BurstBrowser.Display.ContourOffset/100,1,UserValues.BurstBrowser.Display.NumberOfContourLevels);
+    if strcmp(UserValues.BurstBrowser.Display.PlotType,'Scatter')
+        if h.MultiselectOnCheckbox.UserData && numel(n_per_species) > 1
+            color = [];
+            for i = 1:numel(n_per_species)
+                color = [color; i*ones(n_per_species(i),1)];
+            end
+            colors = lines(numel(n_per_species));
+            colordata = colors(color,:);
+            %%% permute data points randomly to avoid hiding populations below another
+            perm = randperm(size(colordata,1));
+            colordata = colordata(perm,:);
+            datapoints = datapoints(perm,:);
+        else
+            colordata = UserValues.BurstBrowser.Display.MarkerColor;
+        end
+        c(3).XData = datapoints(:,1);
+        c(3).YData = datapoints(:,2);
+        c(3).CData = colordata;
+    end
+    xlim(h.axes_lifetime_ind_2d,x_lim);
+    ylim(h.axes_lifetime_ind_2d,y_lim);
+    h.axes_lifetime_ind_2d.CLimMode = 'auto';h.axes_lifetime_ind_2d.CLim(1) = 0;
+    %%% plot the universal circle
+    if ~isempty(strfind(paramX,'gD'))
+        h.axes_lifetime_ind_2d.XLabel.String = 'g_D';
+        h.axes_lifetime_ind_2d.YLabel.String = 's_D';
+        add_universal_circle(h.axes_lifetime_ind_2d,true); % with linker correction
+    else
+        h.axes_lifetime_ind_2d.XLabel.String = 'g_A';
+        h.axes_lifetime_ind_2d.YLabel.String = 's_A';
+        add_universal_circle(h.axes_lifetime_ind_2d);
+    end
+    xdata = xbins;
+    ydata = ybins;
+    zdata = H;
+    origin = []; origin.XLim = x_lim; origin.YLim = y_lim;       
+    
+end
 
 if sum(zdata(:)) == 0
     return;
@@ -17157,3 +17258,68 @@ for b = 1:numel(BurstData)
     end
 end
 Progress(1,h.Progress_Axes,h.Progress_Text);
+
+%%% add universal circle to phasor plot
+function add_universal_circle(ax,linkerwidth)
+global BurstData BurstMeta
+if nargin < 2
+    linkerwidth = false;
+end
+file = BurstMeta.SelectedFile;
+x = 0:0.001:1;
+y = sqrt(0.5^2-(x-0.5).^2);
+axes(ax);hold on;
+plot(x,y,'--r','LineWidth',2);
+
+if linkerwidth %%% also add adjusted universal circle in presence of linker fluctuations
+    R0 = BurstData{file}.Corrections.FoersterRadius;
+    TAC = (BurstData{file}.Phasor.PhasorRange(1)/BurstData{file}.FileInfo.MI_Bins)*BurstData{file}.TACRange*1E9;
+    tauD = BurstData{file}.Corrections.DonorLifetime;
+    sigma = BurstData{file}.Corrections.LinkerLength;
+    if sigma > 0.1
+        [g,s] = universal_circle_linker(R0,sigma,tauD,TAC);
+        plot(g,s,'--k','LineWidth',2);
+    end
+end
+
+
+%%% universal circle in presence of linker fluctuations
+function [g,s] = universal_circle_linker(R0,s,tauD,TAC)
+res = 1000;
+omega = 2*pi/TAC;
+%range of RDA center values, i.e. 1000 values in 0*R0 to 3*R0
+R = linspace(0*R0,3*R0,res);
+
+%for every R calculate gaussian distribution
+p = zeros(numel(R),res);
+r = zeros(numel(R),res);
+for j = 1:numel(R)
+    x = linspace(R(j)-4*s,R(j)+4*s,res);
+    dummy = exp(-((x-R(j)).^2)./(2*s^2));
+    dummy(x < 0) = 0;
+    dummy = dummy./sum(dummy);
+    p(j,:) = dummy;
+    r(j,:) = x;
+end
+
+%calculate lifetime distribution
+tau = zeros(numel(R),res);
+for j = 1:numel(R)
+    tau(j,:) = tauD./(1+((R0./r(j,:)).^6));
+end
+
+% perform intensity weighting of contributions
+for j = 1:numel(R)
+    p(j,:) = p(j,:).*tau(j,:)./sum(p(j,:).*tau(j,:));
+end
+
+%calculate intensity-averaged g and s values
+g = zeros(1,numel(R));
+for j = 1:numel(R)
+    g(j) = sum(p(j,:).*(1./(1+tau(j,:).^2.*omega^2)));
+end
+
+s = zeros(1,numel(R));
+for j = 1:numel(R)
+    s(j) = sum(p(j,:).*((tau(j,:).*omega)./(1+tau(j,:).^2.*omega^2)));
+end
