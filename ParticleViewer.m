633759a5e9073fcfd005a747c6ae1b4b93bf5f05
Anders <anders.barth@gmail.com>
2018-09-21 13:25:05 +0200

    Fixed toolbar behavior for MATLAB 2018b and onwards.

--- ParticleViewer.m
+++ ParticleViewer.m
@@ -38,6 +38,10 @@ toolbar = findall(h.ParticleViewer,'Type','uitoolbar');
 toolbar_items = findall(toolbar);
 if verLessThan('matlab','9.5') %%% toolbar behavior changed in MATLAB 2018b
     delete(toolbar_items([2:7 9 13:17]));
+else %%% 2018b and upward
+    %%% just remove the tool bar since the options are now in the axis
+    %%% (e.g. axis zoom etc)
+    delete(toolbar_items);
 end
 
 h.Load_Particle_Data = uimenu(...
