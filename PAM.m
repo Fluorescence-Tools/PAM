21dbf14244e17e311b7173762159713448f89a22
Anders <anders.barth@gmail.com>
2018-09-21 13:23:53 +0200

    Fixed toolbar behavior for MATLAB 2018b and onwards.

--- PAM.m
+++ PAM.m
@@ -70,7 +70,12 @@ toolbar = findall(h.Pam,'Type','uitoolbar');
 toolbar_items = findall(toolbar);
 if verLessThan('matlab','9.5') %%% toolbar behavior changed in MATLAB 2018b
     delete(toolbar_items([2:7 9 13:17]));
+else %%% 2018b and upward
+    %%% just remove the tool bar since the options are now in the axis
+    %%% (e.g. axis zoom etc)
+    delete(toolbar_items);
 end
+
 %%% Sets background of axes and other things
 whitebg(Look.Axes);
 %%% Changes Pam background; must be called after whitebg
