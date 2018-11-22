4fd83c0cbda5c574690081071d5f5d99a86d8e06
Anders <anders.barth@gmail.com>
2018-11-22 10:08:10 +0100

    BurstBrowser: Split plot function for kinetic consistency check into separate files.

---
+++ functions/BurstBrowser/kinetic_consistency_check/plot_BVA.m
@@ -0,0 +1,40 @@
+function plot_BVA(E,sSelected,BinCenters,sPerBin)
+global UserValues
+h = guidata(findobj('Tag','BurstBrowser'));
+
+%%% create BVA plot
+hfig = figure('color',[1 1 1]);a=gca;a.FontSize=14;a.LineWidth=1.0;a.Color =[1 1 1];
+hold on;
+X_expectedSD = linspace(0,1,1000);          
+xlabel('Proximity Ratio, E*'); 
+ylabel('SD of E*, s');
+BinCenters = BinCenters';
+sigm = sqrt(X_expectedSD.*(1-X_expectedSD)./UserValues.BurstBrowser.Settings.PhotonsPerWindow_BVA);
+
+[H,x,y] = histcounts2(E,sSelected,UserValues.BurstBrowser.Display.NumberOfBinsX);
+
+switch UserValues.BurstBrowser.Display.PlotType
+    case 'Contour'
+    % contourplot of per-burst STD
+        contourf(x(1:end-1),y(1:end-1),H','LevelList',max(H(:))*linspace(UserValues.BurstBrowser.Display.ContourOffset/100,1,UserValues.BurstBrowser.Display.NumberOfContourLevels),'EdgeColor','none');
+        axis('xy')
+        caxis([0 max(H(:))*UserValues.BurstBrowser.Display.PlotCutoff/100]);
+    case 'Image'       
+        Alpha = H./max(max(H)) > UserValues.BurstBrowser.Display.ImageOffset/100;
+        imagesc(x(1:end-1),y(1:end-1),H','AlphaData',Alpha');axis('xy');     
+        %imagesc(x(1:end-1),y(1:end-1),H','AlphaData',isfinite(H));axis('xy');
+        caxis([UserValues.BurstBrowser.Display.ImageOffset/100 max(H(:))*UserValues.BurstBrowser.Display.PlotCutoff/100]);
+    case 'Scatter'
+        scatter(E,sSelected,'.','CData',UserValues.BurstBrowser.Display.MarkerColor,'SizeData',UserValues.BurstBrowser.Display.MarkerSize);
+    case 'Hex'
+        hexscatter(E,sSelected,'xlim',[-0.1 1.1],'ylim',[0 max(sSelected)],'res',UserValues.BurstBrowser.Display.NumberOfBinsX);
+end        
+patch([-0.1 1.1 1.1 -0.1],[0 0 max(sSelected) max(sSelected)],'w','FaceAlpha',0.2,'edgecolor','none','HandleVisibility','off');
+colormap(hfig,colormap(h.BurstBrowser));
+% plot of expected STD
+plot(X_expectedSD,sigm,'k','LineWidth',1);
+
+% Plot STD per Bin
+sPerBin(sPerBin == 0) = NaN;
+scatter(BinCenters,sPerBin,70,UserValues.BurstBrowser.Display.ColorLine1,'d','filled');
+
---
+++ functions/BurstBrowser/kinetic_consistency_check/plot_E_tau.m
@@ -0,0 +1,26 @@
+function plot_E_tau(E,tauD)
+global UserValues BurstMeta BurstData
+file = BurstMeta.SelectedFile;
+h = guidata(findobj('Tag','BurstBrowser'));
+%%% plot smoothed dynamic FRET line
+[H,x,y] = histcounts2(E,tauD,UserValues.BurstBrowser.Display.NumberOfBinsX,'XBinLimits',[-0.1,1.1],'YBinLimits',[0,1.2]);
+H = H./max(H(:)); %H(H<UserValues.BurstBrowser.Display.ContourOffset/100) = NaN;
+f = figure('Color',[1,1,1],'Position',[100,100,600,600]); hold on;
+contourf(y(1:end-1),x(1:end-1),H,'LevelList',max(H(:))*linspace(UserValues.BurstBrowser.Display.ContourOffset/100,1,UserValues.BurstBrowser.Display.NumberOfContourLevels),'EdgeColor','none');
+colormap(f,colormap(h.BurstBrowser));
+ax = gca;
+ax.CLimMode = 'auto';
+ax.CLim(1) = 0;
+ax.CLim(2) = max(H(:))*UserValues.BurstBrowser.Display.PlotCutoff/100;
+% plot patch to phase contour plot out
+%patch([0,1.2,1.2,0],[-0.1,-0.1,1.1,1.1],[1,1,1],'FaceAlpha',0.5,'EdgeColor','none');
+%%% add static FRET line
+%plot(linspace(0,1,1000),1-linspace(0,1,1000),'-','LineWidth',2,'Color',[0,0,0]);
+plot(BurstMeta.Plots.Fits.staticFRET_EvsTauGG.XData./BurstData{file}.Corrections.DonorLifetime,BurstMeta.Plots.Fits.staticFRET_EvsTauGG.YData,'-','LineWidth',2,'Color',UserValues.BurstBrowser.Display.ColorLine1);
+%plot(BurstMeta.Plots.Fits.dynamicFRET_EvsTauGG(1).XData./BurstData{file}.Corrections.DonorLifetime,BurstMeta.Plots.Fits.dynamicFRET_EvsTauGG(1).YData,'--','LineWidth',2,'Color',UserValues.BurstBrowser.Display.ColorLine1);
+ax.XLim = [0,1.2];
+ax.YLim = [0,1];
+
+xlabel('\tau_{D,A}/\tau_{D,0}');
+ylabel('FRET Efficiency');
+set(gca,'FontSize',24,'LineWidth',2,'Box','on','DataAspectRatio',[1,1,1],'XColor',[0,0,0],'YColor',[0,0,0],'Layer','top');
