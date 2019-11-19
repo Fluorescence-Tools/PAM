0dccf100229c1c35f1eb01ea1f66f7617d5ae67d
Anders <anders.barth@gmail.com>
2019-11-19 14:49:18 +0100

    Bugfix

--- functions/FCSFit/read_kristine_fcs_file.m
+++ functions/FCSFit/read_kristine_fcs_file.m
@@ -3,16 +3,18 @@ function [C, mCountRate, duration] = read_kristine_fcs_file(fname)
 % *.fcs file from the Seidel software package
  if nargin < 1
     return;
+ end
+if exist(fname,'file')
+    % read whole file at once
+    C = dlmread(fname);
+    duration = C(1,3);
+    mCountRate = C(2,3);
+    % check if error given
+    if size(C,2) == 4 %%% error specified
+        C(:,3) = [];
+    elseif size(C,2) == 3 %%% no error specified
+        C(:,3) = 1;
+    end
+else
+    disp(sprintf('File not found: %s',fname));
 end
-fileID = fopen(fname);
-if fileID == -1
-    return;
-end   
-% read whole file at once
-C = textscan(fileID, '%f %f %f %f', 'delimiter','\n');
-fclose(fileID);
-
-duration = C{1,3}(1);
-mCountRate = C{1,3}(2);
-C{3} = [];
-C = horzcat(C{:});
