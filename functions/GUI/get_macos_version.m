70dd7dee71d161d7ea693a9009b32bc90bc1c9b6
Anders <anders.barth@gmail.com>
2019-11-28 10:09:53 +0100

    FCSFit: Replaced function that was unavailable in earlier versions.

--- functions/GUI/get_macos_version.m
+++ functions/GUI/get_macos_version.m
@@ -8,4 +8,4 @@ end
 [~,ver] = system('sw_vers');
 rx = 'ProductVersion:\s*(.*)BuildVersion';
 macos_version = regexp(ver,rx,'tokens');
-macos_version = macos_version{1}{1};
+macos_version = macos_version{1}{1})(1:5); %%% only return major version, i.e. 10.14, 10.15 etc
