18cc4ed681429615ff9f8a0a37551b76b480e452
Anders <anders.barth@gmail.com>
2019-12-06 11:18:22 +0100

    TauFit: Improved global model performance and added support for inhomogenous model.

--- functions/TauFit/fitfun_2dist_donly_global.m
+++ functions/TauFit/fitfun_2dist_donly_global.m
@@ -31,7 +31,7 @@ fraction1 = param(5);
 fraction_donly = param(6);
 sc = param(7);
 bg = param(8);
-R0_unquenched = param(9);
+R0 = param(9);
 tauD01 = param(10);
 tauD01(tauD01==0) = 1; %%% set minimum lifetime to TACbin width
 tauD02 = param(11);
@@ -42,42 +42,86 @@ bg_donly = param(14);
 
 %%% correct Förster radius for quenching
 %%% assume, that the longer donor-only lifetime belongs to the unquenched species
-tauD0 = [tauD01;tauD02];
-R0 = R0_unquenched*ones(2,1);
-quenched = 1 + (tauD01 > tauD02);
-unquenched = 3-quenched;
-R0(quenched) = R0_unquenched*(tauD0(quenched)./tauD0(unquenched)).^(1/6);
+%%% In the following, we assume that the given Förster radius belongs to
+%%% this lifetime.
+if tauD01 > tauD02
+    tau0 = tauD01;
+else
+    tau0 = tauD02;
+end
 
 %%% Determine distribution of lifetimes
-range_lower = min([meanR1-5*sigmaR1,meanR2-5*sigmaR2]);
-range_upper = max([meanR1+5*sigmaR1,meanR2+5*sigmaR2]);
 dR = 0.25;
 xdist = zeros(2,n);
-for j = 1:2
-    switch j
-        case 1
-            meanR = meanR1;
-            sigmaR = sigmaR1;
-            tauD0 = tauD01;
-        case 2
-            meanR = meanR2;
-            sigmaR = sigmaR2;
-            tauD0 = tauD02;
-    end
-    xR = (meanR-4*sigmaR):dR:(meanR+4*sigmaR);
-    xR = xR(xR > 0);
-    pR = (1/(sqrt(2*pi())*sigmaR))*exp(-((xR-meanR).^2)./(2*sigmaR.^2));
 
-    xdist(j,:) = zeros(1,n);
-    for i = 1:numel(xR)
-        xdist(j,:) = xdist(j,:) + pR(i).*...
-            (f1_donly*exp(-((0:n-1)./tauD01).*(1+(R0(1)./xR(i)).^6))+...
-             (1-f1_donly)*exp(-((0:n-1)./tauD02).*(1+(R0(2)./xR(i)).^6)));
+%%% Homogenous model or not?
+homogenous = true;
+if homogenous
+    %%% Assume that the fluorescence properties are identical in the different
+    %%% conformational states, i.e. each states has contributions from both
+    %%% quenched and unquenched donor-only lifetimes.
+    for j = 1:2
+        switch j
+            case 1
+                meanR = meanR1;
+                sigmaR = sigmaR1;            
+            case 2
+                meanR = meanR2;
+                sigmaR = sigmaR2;
+        end
+        xR = (meanR-4*sigmaR):dR:(meanR+4*sigmaR);
+        xR = xR(xR > 0)';
+        pR = (1/(sqrt(2*pi())*sigmaR))*exp(-((xR-meanR).^2)./(2*sigmaR.^2));
+        
+        t = repmat((0:n-1),[numel(pR),1]); % time axis
+        
+        k_RET = (1./tau0).*((R0./xR).^6); % k_RET as a function of R
+        kD1 = (1./tauD01) + repmat(k_RET,[1,size(t,2)]);
+        kD2 = (1./tauD02) + repmat(k_RET,[1,size(t,2)]);
+
+        xdist(j,:) = sum(repmat(pR,[1,size(t,2)]).*...
+                         (f1_donly*exp(-t.*kD1) + ...
+                         (1-f1_donly)*exp(-t.*kD2)),1);
+        xdist(j,:) = xdist(j,:)./sum(pR);
     end
-    xdist(j,:) = xdist(j,:)./sum(pR);
+    xdist = (fraction1*xdist(1,:)+(1-fraction1)*xdist(2,:));
+else
+    %%% Assume that the quenched species belongs to one of the
+    %%% conformational states!
+    %%% In this case, the distribution over the two species (quenched and
+    %%% unqueched) is determined by the fraction of the two conformational
+    %%% states.
+    %%% Fluorescence properties and quenching are coupled.
+    for j = 1:2
+        switch j
+            case 1
+                meanR = meanR1;
+                sigmaR = sigmaR1;
+                tauD0 = tauD01;
+            case 2
+                meanR = meanR2;
+                sigmaR = sigmaR2;
+                tauD0 = tauD02;
+        end
+        xR = (meanR-4*sigmaR):dR:(meanR+4*sigmaR);
+        xR = xR(xR > 0)';
+        pR = (1/(sqrt(2*pi())*sigmaR))*exp(-((xR-meanR).^2)./(2*sigmaR.^2));
+        
+        t = repmat((0:n-1),[numel(pR),1]); % time axis
+        
+        k_RET = (1./tau0).*((R0./xR).^6); % k_RET as a function of R, evaluated based on the respective tau0
+        kD = (1./tauD0) + repmat(k_RET,[1,size(t,2)]);
+
+        xdist(j,:) = sum(repmat(pR,[1,size(t,2)]).*...
+                         exp(-t.*kD),1);
+        xdist(j,:) = xdist(j,:)./sum(pR);
+    end
+    xdist = (fraction1*xdist(1,:)+(1-fraction1)*xdist(2,:));
 end
+
+%%% Add donor only contribution
 xDonly = f1_donly*exp(-(0:n-1)./tauD01)+(1-f1_donly)*exp(-(0:n-1)./tauD02);    
-x = fraction_donly.*xDonly + (1-fraction_donly).*(fraction1*xdist(1,:)+(1-fraction1)*xdist(2,:));
+x = fraction_donly.*xDonly + (1-fraction_donly).*xdist;
 switch conv_type
     case 'linear'
         z = conv(irf, x); z = z(1:n)';
@@ -93,7 +137,6 @@ z = z./sum(z);
 z = z.*(1-bg)+bg./numel(z);z = z.*sum(y(1,:));
 z=z';
 
-% TODO: Separate scatter for donor only
 zDonly = zDonly./sum(zDonly);
 zDonly = (1-sc_donly).*zDonly + sc_donly*Scatter;
 zDonly = zDonly(ignore:end);
@@ -102,3 +145,39 @@ zDonly = zDonly.*(1-bg_donly)+bg_donly./numel(zDonly);
 zDonly = zDonly.*sum(y(2,:));
 
 z = [z zDonly'];
+
+
+%%% old approach with using separate Förster distances
+function old()
+%%% correct Förster radius for quenching
+%%% assume, that the longer donor-only lifetime belongs to the unquenched species
+tauD0 = [tauD01;tauD02];
+tic
+R0_q = R0*ones(2,1);
+quenched = 1 + (tauD01 > tauD02);
+unquenched = 3-quenched;
+R0_q(quenched) = R0*(tauD0(quenched)./tauD0(unquenched)).^(1/6);
+xdist = zeros(2,n);
+for j = 1:2
+    switch j
+        case 1
+            meanR = meanR1;
+            sigmaR = sigmaR1;
+        case 2
+            meanR = meanR2;
+            sigmaR = sigmaR2;
+    end
+    xR = (meanR-4*sigmaR):dR:(meanR+4*sigmaR);
+    xR = xR(xR > 0);
+    pR = (1/(sqrt(2*pi())*sigmaR))*exp(-((xR-meanR).^2)./(2*sigmaR.^2));
+    
+    xdist(j,:) = zeros(1,n);
+    for i = 1:numel(xR)
+        xdist(j,:) = xdist(j,:) + pR(i).*...
+            (f1_donly*exp(-((0:n-1)./tauD01).*(1+(R0_q(1)./xR(i)).^6))+...
+             (1-f1_donly)*exp(-((0:n-1)./tauD02).*(1+(R0_q(2)./xR(i)).^6)));
+    end
+    xdist(j,:) = xdist(j,:)./sum(pR);
+end
+xdist_old = xdist';
+toc
