312e0f1ea78fcda6c5d98411fa0c44c896b8d4d8
Anders <anders.barth@gmail.com>
2020-05-06 19:41:33 +0200

    PDAFit: Fixed background deconvolution not working.

--- PDAFit.m
+++ PDAFit.m
@@ -2606,7 +2606,7 @@ if (any(PDAMeta.PreparationDone(PDAMeta.Active) == 0)) || ~isfield(PDAMeta,'eps_
         eps_grid = linspace(eps_min,1,NobinsE+1);
         if calc
             PDAMeta.P(i,:) = cell(1,NobinsE+1);
-            StasApproach = true;
+            StasApproach = false;
             if StasApproach
                 %%% Note: Background deconvolution is currently NOT supported with this approach!
                 limits = h.AllTab.Main_Axes.XLim;
@@ -2620,7 +2620,7 @@ if (any(PDAMeta.PreparationDone(PDAMeta.Active) == 0)) || ~isfield(PDAMeta,'eps_
                 eps_donor_only = PDAMeta.crosstalk(i)/(1+PDAMeta.crosstalk(i));
                 PDAMeta.P_donly{i} = shot_noise_limited_histogram(eps_donor_only,Nobins+1,maxN,PN,BG,BR,limits,i);
             else
-                [PDAMeta.P(i,:), PDAMeta.P_donly{i}] = generate_histogram_library_matlab(i,NobinsE,Nobins,maxN,PN,h);
+                [PDAMeta.P(i,:), PDAMeta.P_donly{i}] = generate_histogram_library_matlab(i,NobinsE,Nobins,maxN,h);
             end
             PDAMeta.PreparationDone(i) = 1;
         end
@@ -6401,67 +6401,6 @@ PofT = (...
        ((k2*T1+k1*T2)/(k1+k2))*(sqrt(k1*k2)/sqrt(T1*T2))*...
        besseli(1,2*sqrt(k1*k2*T1*T2)) ...
        ) * exp(-k1*T1-k2*T2)*dt;
-     
-function PofF = deconvolute_PofF(S,bg,resolution)
-%%% deconvolutes input PofF using a sum of Poisson distributios as kernel
-%%% to obtain background-free fluorescence signal distribution PofF
-%%%
-%%% See: Kalinin, S., Felekyan, S., Antonik, M. & Seidel, C. A. M. Probability Distribution Analysis of Single-Molecule Fluorescence Anisotropy and Resonance Energy Transfer. J. Phys. Chem. B 111, 10253?10262 (2007).
-
-%%% Input parameter:
-%%% S   -   the experimentally observed burst sizes
-%%% bg  -   the total background
-%%% resolution - resolution for brightness vector
-
-if nargin < 3
-    resolution = 200;
-end
-%%% Construct the histogram PF
-xS = 0:1:max(S)+1;
-PS= histcounts(S,xS);
-xS = xS(1:end-1);
-%%% vector of brightnesses to consider
-b = linspace(0,max(S),resolution);
-
-%%% Establish Poisson library based on brightness vector INCLUDING background
-%%% convolution of model PofF with Poissonian background simplifies to
-%%% using a modified brightness b' = b + bg
-%%% This follows because the convolution of two Poissonian distributions
-%%% equals again a Poissonian with sum of rate parameters.
-%%% see equation 12 of reference
-PS_ind = poisspdf(repmat(xS,numel(b),1),repmat(bg+b',1,numel(xS)));
-
-%%% Calculate error estimate based on poissonian counting statistics
-error = sqrt(PS); error(error == 0) = 1;
-
-%%% equation 12 of reference to calculate PofS from library
-% sum(PS_ind.*repmat(p,1,numel(xS),1)) = P(S)
-%%% scaling parameter for the entropy term
-v = 10;
-mem = @(p) -(v*sum(p-p.*log(p)) - sum( (PS-sum(PS_ind.*repmat(p,1,numel(xS),1)).*numel(S)).^2./error.^2)./(numel(PS)));
-%%% initialize p
-p0 = ones(numel(b),1)./numel(b);
-p=p0;
-
-%%% initialize boundaries
-Aieq = -eye(numel(p0)); bieq = zeros(numel(p0),1);
-lb = zeros(numel(p0),1); ub = inf(numel(p0),1);
-
-%%% specify fit options
-opts = optimoptions(@fmincon,'MaxFunEvals',1E5,'Display','iter','TolFun',1E-3);
-p = fmincon(mem,p,Aieq,bieq,[],[],lb,ub,@nonlcon,opts); 
-
-%%% construct distribution PofF from distribution over brightnesses
-%%% this time, we exculde background to obtain the "purified" fluorescence
-%%% count distribution
-PF_ind = poisspdf(repmat(xS,numel(b),1),repmat(b',1,numel(xS)));
-PofF = sum(PF_ind.*repmat(p,1,numel(xS),1));
-PofF = PofF./sum(PofF);
-
-function [c,ceq] = nonlcon(x)
-%%% nonlinear constraint for deconvolution
-c = [];
-ceq = sum(x) - 1;
 
 function Files = GetMultipleFiles(FilterSpec,Title,PathName)
 FileName = 1;
