990615261d4cd23d0242b53530476c8a76776e16
Anders <anders.barth@gmail.com>
2020-07-13 18:51:20 +0200

    BurstBrowser: Added script to determine normalized dynamic shift.

---
+++ functions/BurstBrowser/unimplemented/get_dynamic_shift.m
@@ -0,0 +1,29 @@
+%%% calculates the dynamic shift from the data marker placed on the peak of
+%%% the population in the E-tau plot
+global BurstMeta BurstData
+h = guidata(findobj('Tag','BurstBrowser'));
+
+% get tauD0
+tauD0 = BurstData{BurstMeta.SelectedFile}.Corrections.DonorLifetime;
+
+f = figure;
+copyobj(h.axes_lifetime_ind_2d,f);
+set(gca,'Color',[1,1,1],'XColor',[0,0,0],'YColor',[0,0,0],'Position',[0.15,0.15,0.65,0.8]);
+ax = gca;
+
+
+dcm = datacursormode;
+disp('Click line to display a data tip, then press "Return"')
+pause 
+point = dcm.getCursorInfo.Position(1:2);
+point(1) = point(1)./tauD0;
+
+% get the static FRETline
+x_line = BurstMeta.Plots.Fits.staticFRET_EvsTauGG.XData./tauD0;
+y_line = BurstMeta.Plots.Fits.staticFRET_EvsTauGG.YData;
+
+% find closest distance = dynamic shift
+d = sqrt((x_line-point(1)).^2+(y_line-point(2)).^2);
+ds = min(d);
+
+fprintf('Dynamic shift: %.3f\n',ds);
