7ccfa42d848a4492f74b65a41d440dc6270b8bc7
Simon Wanninger <simon@xnull.de>
2020-07-30 20:20:34 +0200

    BurstBrowser CA: fixed legend entry

--- functions/BurstBrowser/ConsistencyAnalysis.m
+++ functions/BurstBrowser/ConsistencyAnalysis.m
@@ -455,11 +455,11 @@ switch UserValues.BurstBrowser.Settings.Dynamic_Analysis_Method
         w_res_dyn = (mean_tauD-mean_tauD_sim);
         w_res_dyn(isnan(w_res_dyn)) = 0;
         
-        SSR_Model1_legend = ['SIM data' newline 'SSR:' ' ' sprintf('%.0e',round(sum(w_res_dyn.^2),1,'significant'))];
+        SSR_Model1_legend = ['Model 1' newline 'SSR:' ' ' sprintf('%.0e',round(sum(w_res_dyn.^2),1,'significant'))];
 %         maxXLim = [0 max([tauD;tauD_sim'])+0.01];
         switch UserValues.BurstBrowser.Settings.BVA_ModelComparison
             case 1
-                %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+                %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%s%%%%%%%%%%%%%
                 %%%%%%% Compare two models to experimental data %%%%%%%
                 %%%%%%% and plot separately                     %%%%%%%
                 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
