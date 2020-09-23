b53563d8a4293e822fe64fb1185e1bc99ebce9e7
Anders <anders.barth@gmail.com>
2020-09-23 15:17:24 +0200

    BurstBrowser: Added normalized deviation to static FRET line.

--- functions/BurstBrowser/check_gamma_beta_consistency.m
+++ functions/BurstBrowser/check_gamma_beta_consistency.m
@@ -21,8 +21,7 @@ end
 
 if any(mode == 2) %%% static FRET-line deviation
     E = get_multiselection_data(h,'FRET Efficiency');
-    tau = get_multiselection_data(h,'Lifetime D [ns]');
-    N_GX = get_multiselection_data(h,'Number of Photons (DD)') + get_multiselection_data(h,'Number of Photons (DA)');
+    tau = get_multiselection_data(h,'Lifetime D [ns]');    
     %%% deviation from E-tau line
     % only makes sense if lifetime is available
     if ~all(tau==0)
@@ -30,13 +29,17 @@ if any(mode == 2) %%% static FRET-line deviation
         [~,staticFRETline,~] = conversion_tau(BurstData{file}.Corrections.DonorLifetime,...
             BurstData{file}.Corrections.FoersterRadius,BurstData{file}.Corrections.LinkerLength);
         E_model = staticFRETline(tau);
+        valid = E > 0 & E <1 & tau > 0 & tau <= BurstData{file}.Corrections.DonorLifetime & ~isnan(tau) & ~isnan(E_model);
+        RMSE_tau = sqrt(sum((E(valid)-E_model(valid)).^2)/sum(valid));
         % convert to proximity ratio to have the correct noise characteristics
-        FRETtoPR = @(E) 1-(1-E)./(1+BurstData{file}.Corrections.CrossTalk_GR+BurstData{file}.Corrections.DirectExcitation_GR*BurstData{file}.Corrections.Gamma_GR*BurstData{file}.Corrections.Beta_GR-...
-                    (1+BurstData{file}.Corrections.CrossTalk_GR-BurstData{file}.Corrections.Gamma_GR).*E);
-        E = FRETtoPR(E);
-        E_model = FRETtoPR(E_model);
-        var_E = E.*(1-E)./N_GX;
-        RMSE_tau = sqrt(sum((E-E_model).^2./var_E,'omitnan')/numel(E));
+        %FRETtoPR = @(E) 1-(1-E)./(1+BurstData{file}.Corrections.CrossTalk_GR+BurstData{file}.Corrections.DirectExcitation_GR*BurstData{file}.Corrections.Gamma_GR*BurstData{file}.Corrections.Beta_GR-...
+        %            (1+BurstData{file}.Corrections.CrossTalk_GR-BurstData{file}.Corrections.Gamma_GR).*E);
+        %E = FRETtoPR(E);
+        %E_model = FRETtoPR(E_model);
+        %N_GX = get_multiselection_data(h,'Number of Photons (DD)') + get_multiselection_data(h,'Number of Photons (DA)');
+        %sigma_E = sqrt(E.*(1-E)./N_GX);
+        %valid = E > 0 & E <1 & tau > 0 & tau <= BurstData{file}.Corrections.DonorLifetime & ~isnan(tau) & ~isnan(E_model);
+        %RMSE_tau = sqrt(sum(((E(valid)-E_model(valid))./sigma_E(valid)).^2)/sum(valid));
         fprintf('Root mean square error to static FRET-line:\t%.5f\n',RMSE_tau);
     end
 end
