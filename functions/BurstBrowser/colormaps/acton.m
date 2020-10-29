339aab1864c6b574945d15dd9117e8c3e3880a4c
Anders <anders.barth@gmail.com>
2020-10-29 14:35:55 +0100

    BurstBrowser: Added code for new colormaps.

---
+++ functions/BurstBrowser/colormaps/acton.m
@@ -0,0 +1,14 @@
+function [cm_data] = acton(m)
+
+cm = load('ScientificColourMaps6/acton.mat');
+cm = cm.acton;
+
+if nargin < 1
+    cm_data = cm;
+else
+    hsv=rgb2hsv(cm);
+    hsv(170:end,1)=hsv(170:end,1)+1; % hardcoded
+    cm_data=interp1(linspace(0,1,size(cm,1)),hsv,linspace(0,1,m));
+    cm_data(cm_data(:,1)>1,1)=cm_data(cm_data(:,1)>1,1)-1;
+    cm_data=hsv2rgb(cm_data);
+end
