b98fff561d3c6e8f18196fcfcf029a6e189c3abb
Anders <anders.barth@gmail.com>
2020-11-01 12:08:19 +0100

    BurstBrowser: Fixed some colormaps.

--- functions/BurstBrowser/colormaps/davos.m
+++ functions/BurstBrowser/colormaps/davos.m
@@ -1,14 +1,18 @@
 function [cm_data] = davos(m)
-
+if nargin < 1
+    m = [];
+end
+    
 cm = load('ScientificColourMaps6/davos.mat');
 cm = cm.davos;
 
-if nargin < 1
-    cm_data = cm;
-else
-    hsv=rgb2hsv(cm);
-    hsv(170:end,1)=hsv(170:end,1)+1; % hardcoded
-    cm_data=interp1(linspace(0,1,size(cm,1)),hsv,linspace(0,1,m));
-    cm_data(cm_data(:,1)>1,1)=cm_data(cm_data(:,1)>1,1)-1;
-    cm_data=hsv2rgb(cm_data);
-end
+cm_data = cm;
+% if nargin < 1
+%     cm_data = cm;
+% else
+%     hsv=rgb2hsv(cm);
+%     hsv(170:end,1)=hsv(170:end,1)+1; % hardcoded
+%     cm_data=interp1(linspace(0,1,size(cm,1)),hsv,linspace(0,1,m));
+%     cm_data(cm_data(:,1)>1,1)=cm_data(cm_data(:,1)>1,1)-1;
+%     cm_data=hsv2rgb(cm_data);
+% end
--- functions/BurstBrowser/colormaps/hawaii.m
+++ functions/BurstBrowser/colormaps/hawaii.m
@@ -1,14 +1,18 @@
 function [cm_data] = hawaii(m)
+if nargin < 1
+    m = [];
+end
 
 cm = load('ScientificColourMaps6/hawaii.mat');
 cm = cm.hawaii;
 
-if nargin < 1
-    cm_data = cm;
-else
-    hsv=rgb2hsv(cm);
-    hsv(170:end,1)=hsv(170:end,1)+1; % hardcoded
-    cm_data=interp1(linspace(0,1,size(cm,1)),hsv,linspace(0,1,m));
-    cm_data(cm_data(:,1)>1,1)=cm_data(cm_data(:,1)>1,1)-1;
-    cm_data=hsv2rgb(cm_data);
-end
+cm_data = cm;
+% if nargin < 1
+%     cm_data = cm;
+% else
+%     hsv=rgb2hsv(cm);
+%     hsv(170:end,1)=hsv(170:end,1)+1; % hardcoded
+%     cm_data=interp1(linspace(0,1,size(cm,1)),hsv,linspace(0,1,m));
+%     cm_data(cm_data(:,1)>1,1)=cm_data(cm_data(:,1)>1,1)-1;
+%     cm_data=hsv2rgb(cm_data);
+% end
