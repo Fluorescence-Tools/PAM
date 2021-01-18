47d686523a8bb5133dfd8a00efcc60e7170883e0
Anders <anders.barth@gmail.com>
2021-01-18 18:27:47 +0100

    BurstBrowser: Some updates to Gopich-Szabo MLE method.

--- functions/BurstBrowser/GS_likelihood/fit_Likelihood.m
+++ functions/BurstBrowser/GS_likelihood/fit_Likelihood.m
@@ -1,14 +1,25 @@
 global BurstData BurstTCSPCData BurstMeta
 file = BurstMeta.SelectedFile;
+if isempty(BurstTCSPCData{file})
+    Load_Photons();
+end
 mt = BurstTCSPCData{file}.Macrotime(BurstData{file}.Selected);
 ch = BurstTCSPCData{file}.Channel(BurstData{file}.Selected);
 % convert channel to color and discard RR photons
+switch BurstData{file}.BAMethod
+    case {1,2}
+        RRix = 5;
+    case 5 % noMFD
+        RRix = 3;
+end
 for i = 1:numel(mt)
     % discard RR
-    mt{i} = double(mt{i}(ch{i} < 5));
-    ch{i} = double(ch{i}(ch{i} < 5));
+    mt{i} = double(mt{i}(ch{i} < RRix));
+    ch{i} = double(ch{i}(ch{i} < RRix));
     % convert channel to color (1->D; 2->A)
-    ch{i} = ceil(ch{i}/2);
+    if any(BurstData{file}.BAMethod == [1,2])
+        ch{i} = ceil(ch{i}/2);
+    end
 end
 mt = cellfun(@(x) x*BurstData{file}.SyncPeriod,mt,'UniformOutput',false);
 
@@ -20,11 +31,11 @@ switch n_states
 
         % initial values
         % set rates to 1/100mus
-        k0 = [1,1]*1E1;
-        E0 = [0.2,0.8];
+        k0 = [0.1,0.1]*1E3;
+        E0 = 1./(1+([60,40]./50).^6);%[0.25,0.8];
         lb = [0,0,0,0];
-        ub = [1E5,1E5,1,1];
-        fixE = false;
+        ub = [1E6,1E6,1,1];
+        fixE = true;
         if fixE
             lb(end-1:end) = E0;
             ub(end-1:end) = E0;   
@@ -35,12 +46,12 @@ switch n_states
 
         % initial values
         % set rates to 1/1ms
-        k0 = [1,1,1,1,1,1]*1E2;
-        E0 = [0.06,0.36,0.76];
+        k0 = [1,1,1,1,1,1]*1E3;
+        E0 =  1./(1+([60,50,40]./50).^6);%[0.25,0.525,0.8];
         
         lb = zeros(1,numel(k0)+numel(E0));
-        % maximum rate of 1/100µs
-        ub = [1E4*ones(1,numel(k0)),ones(1,numel(E0))];
+        % maximum rate of 1E6 Hz
+        ub = [1E6*ones(1,numel(k0)),ones(1,numel(E0))];
             
         fixE = true;
         if fixE
@@ -67,11 +78,11 @@ if vit
 end
 
 trans = false;
-if trans & n_states == 2
+if trans && n_states == 2
     % compare logL of model with transition time over a range
     % logL of tp = 0
     logL0 = GP_logL_burst(mt,ch,fitres(1:2),fitres(3:4));
-    tp = logspace(-6,-2,15); % from 1 to 100 µs
+    tp = logspace(-6,-3,20); % from 1 to 1000 µs
     kT = 1./(2*tp);
     logL = zeros(1,numel(kT));
     for i = 1:numel(kT)
--- functions/BurstBrowser/unimplemented/burstwise_traces.m
+++ functions/BurstBrowser/unimplemented/burstwise_traces.m
@@ -1,29 +1,72 @@
-global BurstTCSPCData
-global BurstData
-
-selected = BurstData.Selected;
-Mac = BurstTCSPCData.Macrotime(selected);
-Chan = BurstTCSPCData.Channel(selected);
-
-for i = 1:numel(Mac)
-    mBB{i} = Mac{i}((Chan{i} == 1) | (Chan{i}== 2));
-    mBG{i} = Mac{i}((Chan{i} == 3) | (Chan{i}== 4));
-    mBR{i} = Mac{i}((Chan{i} == 5) | (Chan{i}== 6));
-    mBBt{i} =  mBB{i}.*BurstData.ClockPeriod.*1000;
-    mBGt{i} =  mBG{i}.*BurstData.ClockPeriod.*1000;
-    mBRt{i} =  mBR{i}.*BurstData.ClockPeriod.*1000;
-    
-    start{i} = min([mBBt{i}; mBGt{i}; mBRt{i}]);
-    mBBt{i} = mBBt{i}-start{i};
-    mBGt{i} = mBGt{i}-start{i};
-    mBRt{i} = mBRt{i}-start{i};
-
-    %histogram in steps of 1 ms
-    maxi(i)=max([max(mBBt{i}) max(mBGt{i}) max(mBRt{i})]);
-    hBB{i} = hist(mBBt{i},0:1:maxi(i));
-    hBG{i} = hist(mBGt{i},0:1:maxi(i));
-    hBR{i} = hist(mBRt{i},0:1:maxi(i));
+global BurstTCSPCData BurstData BurstMeta
+file = BurstMeta.SelectedFile;
+if isempty(BurstTCSPCData{file})
+    Load_Photons();
 end
+selected = BurstData{file}.Selected;
+Mac = BurstTCSPCData{file}.Macrotime(selected);
+Chan = BurstTCSPCData{file}.Channel(selected);
 
-figure;plot(hBB{i},'b');hold on;plot(hBG{i},'g');plot(hBR{i},'r')
-figure;plot(hBR{i}./(hBB{i}+hBG{i}+hBR{i}),'r');hold on;plot(hBG{i}./(hBB{i}+hBG{i}+hBR{i}),'Color',[0 0.2 0.8])
+switch BurstData{file}.BAMethod
+    case {1,2,5}
+        switch BurstData{file}.BAMethod
+            case {1,2} % MFD
+                for i = 1:numel(Mac)
+                    mGG{i} = Mac{i}((Chan{i} == 1) | (Chan{i}== 2));
+                    mGR{i} = Mac{i}((Chan{i} == 3) | (Chan{i}== 4));
+                    mRR{i} = Mac{i}((Chan{i} == 5) | (Chan{i}== 6));                    
+                end
+            case 5 % noMFD
+                for i = 1:numel(Mac)
+                    mGG{i} = Mac{i}((Chan{i} == 1));
+                    mGR{i} = Mac{i}((Chan{i} == 2));
+                    mRR{i} = Mac{i}((Chan{i} == 3));
+                end                   
+        end
+        mGG = cellfun(@double,mGG,'UniformOutput',false);
+        mGR = cellfun(@double,mGR,'UniformOutput',false);
+        mRR = cellfun(@double,mRR,'UniformOutput',false);
+        for i = 1:numel(Mac) 
+            mGGt{i} =  mGG{i}.*BurstData{file}.ClockPeriod.*1000;
+            mGRt{i} =  mGR{i}.*BurstData{file}.ClockPeriod.*1000;
+            mRRt{i} =  mRR{i}.*BurstData{file}.ClockPeriod.*1000;
+            
+            start{i} = min([mGGt{i}; mGRt{i}; mRRt{i}]);
+            mGGt{i} = mGGt{i}-start{i};
+            mGRt{i} = mGRt{i}-start{i};
+            mRRt{i} = mRRt{i}-start{i};
+            
+            %histogram in steps of 1 ms
+            timebin = 0.1;
+            maxi(i) = max([max(mGGt{i}) max(mGRt{i}) max(mRRt{i})]);
+            hGG{i} = hist(mGGt{i},0:timebin:maxi(i));
+            hGR{i} = hist(mGRt{i},0:timebin:maxi(i));
+            hRR{i} = hist(mRRt{i},0:timebin:maxi(i));
+        end
+        i = 1;
+        figure;plot(hGG{i},'b');hold on;plot(hGR{i},'g');%plot(hRR{i},'r');
+        figure;plot(hGR{i}./(hGG{i}+hGR{i}),'r');
+    case {3,4}
+        for i = 1:numel(Mac)
+            mBB{i} = Mac{i}((Chan{i} == 1) | (Chan{i}== 2));
+            mBG{i} = Mac{i}((Chan{i} == 3) | (Chan{i}== 4));
+            mBR{i} = Mac{i}((Chan{i} == 5) | (Chan{i}== 6));
+            mBBt{i} =  mBB{i}.*BurstData{file}.ClockPeriod.*1000;
+            mBGt{i} =  mBG{i}.*BurstData{file}.ClockPeriod.*1000;
+            mBRt{i} =  mBR{i}.*BurstData{file}.ClockPeriod.*1000;
+
+            start{i} = min([mBBt{i}; mBGt{i}; mBRt{i}]);
+            mBBt{i} = mBBt{i}-start{i};
+            mBGt{i} = mBGt{i}-start{i};
+            mBRt{i} = mBRt{i}-start{i};
+
+            %histogram in steps of 1 ms
+            maxi(i)=max([max(mBBt{i}) max(mBGt{i}) max(mBRt{i})]);
+            hBB{i} = hist(mBBt{i},0:1:maxi(i));
+            hBG{i} = hist(mBGt{i},0:1:maxi(i));
+            hBR{i} = hist(mBRt{i},0:1:maxi(i));
+        end
+        i = 1;
+        figure;plot(hBB{i},'b');hold on;plot(hBG{i},'g');plot(hBR{i},'r');
+        figure;plot(hBR{i}./(hBB{i}+hBG{i}+hBR{i}),'r');hold on;plot(hBG{i}./(hBB{i}+hBG{i}+hBR{i}),'Color',[0 0.2 0.8]);
+end
